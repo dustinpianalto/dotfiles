d93666e7c4bd5e0d52bd828d32733df977f18e47
Dusty Pianalto <dustin@djpianalto.com>
2019-12-02 22:23:13 -0900

    Update Ubuntu install script to install pyenv

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -13,9 +13,9 @@ sudo apt install terminator -y
 
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
 
-[[ ! -d "~/.pyenv" ]] && curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | /usr/bin/zsh
+[[ ! -d "$HOME/.pyenv" ]] && curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | /usr/bin/zsh
 
-export PATH="~/.pyenv/bin:$PATH"
+export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
@@ -43,16 +43,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # Symlink dotfiles. This allows updates to dotfiles to be automatically applied
 # just by pulling an update from this repo
-[[ ! -d "~/.config/terminator" ]] && mkdir -p ~/.config/terminator
-[[ -a "~/.config/terminator/config" ]] && mv ~/.config/terminator/config ~/.config/terminator/config.old
+[[ ! -d "$HOME/.config/terminator" ]] && mkdir -p ~/.config/terminator
+[[ -a "$HOME/.config/terminator/config" ]] && mv ~/.config/terminator/config ~/.config/terminator/config.old
 ln -s ./terminator/config ~/.config/terminator/config
 
-[[ -a "~/.zshrc" ]] && mv ~/.zshrc ~/.zshrc.old
+[[ -a "$HOME/.zshrc" ]] && mv ~/.zshrc ~/.zshrc.old
 ln -s ./zsh/.zshrc ~/.zshrc
 
-[[ -a "~/.p10k.zsh" ]] && mv ~/.p10k.zsh ~/.p10k.zsh.old
+[[ -a "$HOME/.p10k.zsh" ]] && mv ~/.p10k.zsh ~/.p10k.zsh.old
 ln -s ./zsh/.p10k.zsh ~/.p10k.zsh
 
-[[ ! -d "~/.config/nvim" ]] && mkdir -p ~/.config/nvim
-[[ -a "~/.config/nvim/init.vim" ]] && mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
+[[ ! -d "$HOME/.config/nvim" ]] && mkdir -p ~/.config/nvim
+[[ -a "$HOME/.config/nvim/init.vim" ]] && mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 ln -s ./neovim/init.vim ~/.config/nvim/init.vim
